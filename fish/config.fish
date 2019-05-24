94741a29e26c0cb32137ec2ac3f39a54881fac95
Carter McBride <junk@carter.works>
2019-05-24 10:21:14 -0600

    term - add kitty shell completions

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/cmcbride/bin/google-cloud-sdk/path.fish.inc' ]; . '/Users/cmcbride/bin/google-cloud-sdk/path.fish.inc'; end
-
+# Add kitty shell integrations
