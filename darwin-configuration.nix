bf168c6b5b00a442ec39f6f415a0e713b8e402e4
Carter McBride <=>
2022-08-08 08:49:06 -0600

    (nix-darwin) enable flakes

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -28,6 +28,9 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
