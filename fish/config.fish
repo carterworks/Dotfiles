3dafa070f23f5c75b7bdd5508a116c731bd13b9e
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-06-17 10:12:02 -0600

    [pnpm,fish] add completions

--- fish/config.fish
+++ fish/config.fish
@@ -20,7 +20,7 @@ end
 if command -qs eza
     set -gx EZA_ICONS_AUTO true
     set -gx EZA_ICON_SPACING 2
-    alias ls="eza --classify=auto --hyperlink --group-directories-first"
+    alias ls="eza --classify=auto --group-directories-first"
 end
 set -gx base16_fish_theme selenized-light
 if test -n "$base16_fish_theme" && status --is-interactive
@@ -32,3 +32,19 @@ end
 
 # homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# pnpm
+if command -qs pnpm
+    set -gx PNPM_HOME "/Users/cmcbride/Library/pnpm"
+    if not string match -q -- $PNPM_HOME $PATH
+        set -gx PATH "$PNPM_HOME" $PATH
+    end
+    # completions
+    if status --is-interactive
+        # Only source the definitions once per session.
+        if not functions -q _pnpm_completion
+            pnpm completion fish | source
+        end
+    end
+end
+# pnpm end
