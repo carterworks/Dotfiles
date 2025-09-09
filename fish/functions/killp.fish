1e4947ed9bee62df3ce7721e14c517e61df76c82
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-09-09 14:57:54 -0600

    Add killp function for interactive process killing with fzf

---
+++ fish/functions/killp.fish
@@ -0,0 +1,35 @@
+function killp --description "Select processes to kill using fzf"
+    set -l fzf_args \
+        --multi \
+        --preview 'ps -p {2} -o pid,ppid,user,comm,args,pcpu,pmem,etime,stat' \
+        --preview-label='alt-p: toggle preview, alt-j/k: scroll, tab: multi-select, enter: kill selected' \
+        --preview-label-pos='bottom' \
+        --preview-window 'down:65%:wrap' \
+        --bind 'alt-p:toggle-preview' \
+        --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up' \
+        --bind 'alt-k:preview-up,alt-j:preview-down' \
+        --color 'pointer:red,marker:red' \
+        --header 'Select processes to kill (WARNING: This will terminate selected processes)'
+
+    # Get process list with PID as second column for easy extraction
+    set -l selected_processes (ps -eo pid,comm,user,pcpu,pmem,etime,args --sort=-pcpu | \
+        tail -n +2 | \
+        fzf $fzf_args | \
+        awk '{print $1}')
+
+    if test -n "$selected_processes"
+        echo "Selected PIDs to kill: $selected_processes"
+        echo -n "Confirm killing these processes? [y/N] "
+        read -l confirm
+        
+        if test "$confirm" = "y" -o "$confirm" = "Y"
+            for pid in $selected_processes
+                echo "Killing PID $pid..."
+                kill $pid
+            end
+            echo "Done."
+        else
+            echo "Cancelled."
+        end
+    end
+end
