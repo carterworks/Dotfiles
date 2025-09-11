611b253a7c6abc4251869813a5386ef0a33ebb5c
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-09-11 12:49:50 -0600

    Move fish functions to be bin scripts

--- fish/functions/ls.fish
+++
@@ -1,7 +0,0 @@
-function ls --wraps='eza -F --group-directories-first' --description 'alias ls=eza -F --group-directories-first'
-  if command -qs eza
-    eza -F --group-directories-first $argv
-  else
-    /bin/ls $argv
-  end      
-end
