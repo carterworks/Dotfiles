87f7b87e1b4f8ecabeef5b1d4916b86f792c2c12
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-02-12 14:56:23 -0700

    merge in nixos config into flake

---
+++ nixos/configuration.nix
@@ -0,0 +1,295 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  # Disable the nixpkgs elephant module since walker provides its own
+  disabledModules = [ "services/misc/elephant.nix" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostId = "8425e349";
+  networking.hostName = "scylla"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Enable flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable NVidia drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     tree
+  #   ];
+  # };
+  nixpkgs.config.allowUnfree = true;
+  users.groups.games = { };
+  users.users.carter = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "games"
+    ];
+    packages = with pkgs; [
+      atuin
+      bat
+      btop
+      delta
+      dust
+      eza
+      fastfetch
+      fd
+      fish
+      fzf
+      gh
+      git
+      gum
+      hyperfine
+      iosevka-bin
+      inter
+      papirus-icon-theme
+      nerd-fonts.iosevka
+      opencode
+      jq
+      jujutsu
+      sshpass
+      rclone
+      ripgrep
+      rsync
+      starship
+      uv
+      wallust
+      yazi
+      zed-editor
+      zoxide
+      # hyprland/desktop env stuff
+      ashell
+      ghostty
+      bibata-cursors
+      mako
+      nautilus
+      hyprpaper
+      hyprpolkitagent
+      hyprpicker
+      hyprshutdown
+      wl-clipboard
+      wlogout
+      grim
+      slurp
+      # GUI apps
+      discord
+      gearlever
+      google-chrome
+      obsidian
+      spotify
+      telegram-desktop
+      heroic
+      trayscale
+    ];
+  };
+  environment.sessionVariables = {
+    XCURSOR_THEME = "Bibata-Modern-Classic";
+    XCURSOR_SIZE = "24";
+    HYPRCURSOR_SIZE = "Bibata-Modern-Classic";
+    HYPRCURSOR_THEME = "24";
+    UV_PYTHON = "${pkgs.python3}/bin/python3";
+    UV_PYTHON_DOWNLOADS = "never";
+  };
+  programs.steam = {
+    enable = true;
+  };
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.syncthing = {
+    enable = true;
+    user = "carter";
+    dataDir = "/home/carter/syncthing";
+    configDir = "/home/carter/.config/syncthing";
+    openDefaultPorts = true;
+    # TODO: add make these true later
+    overrideDevices = false;
+    overrideFolders = false;
+  };
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      hyprland.default = [
+        "gtk"
+        "hyprland"
+      ];
+    };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+  services.displayManager = {
+    autoLogin.enable = true;
+    autoLogin.user = "carter";
+    sddm.enable = true;
+    sddm.wayland.enable = true;
+  };
+  programs.dconf.profiles.user.databases = [
+    {
+      lockAll = true;
+      settings = {
+        "org/gnome/desktop/interface" = {
+          "color-scheme" = "prefer-light";
+          "icon-theme" = "Papirus";
+        };
+      };
+    }
+  ];
+  # https://wiki.hypr.land/Nix/Hyprland-on-NixOS/#fixing-problems-with-themes
+  # programs.dconf.profiles.user.databases = [
+  #   {
+  #     settings."org/gnome/desktop/interface" = {
+  #       gtk-theme = "Adwaita";
+  #       icon-theme = "Flat-Remix-Red-Dark";
+  #       font-name = "Noto Sans Medium 11";
+  #       document-font-name = "Noto Sans Medium 11";
+  #       monospace-font-name = "Noto Sans Mono Medium 11";
+  #     };
+  #   }
+  # ];
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    bun
+    cifs-utils
+    curl
+    helix
+    nodejs
+    python3
+    samba
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.gvfs.enable = true;
+
+  systemd.tmpfiles.rules = [
+    "d /games 2775 root games -"
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable binary caches
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+    ];
+  };
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
