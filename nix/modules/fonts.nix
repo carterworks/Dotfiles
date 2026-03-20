5d9f35f731fa30089647694f212fe3d1935dd6dc
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-03-19 20:28:01 -0600

    [nix] integrate stylix

--- nix/modules/fonts.nix
+++ nix/modules/fonts.nix
@@ -2,13 +2,11 @@
 
 {
   fonts.packages = [
-    pkgs.iosevka-bin
   ]
   ++ lib.optionals pkgs.stdenv.isDarwin [
     pkgs.nerd-fonts.noto
   ]
   ++ lib.optionals pkgs.stdenv.isLinux [
-    pkgs.inter
     pkgs.nerd-fonts.iosevka
   ];
 }
