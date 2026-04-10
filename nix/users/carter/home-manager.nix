0a07faa652a83ed125041df5839b489afe5d9132
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-04-09 20:29:01 -0600

    [pi] fff-mcp tool as find,grep

--- nix/users/carter/home-manager.nix
+++ nix/users/carter/home-manager.nix
@@ -63,6 +63,11 @@
           fish_add_path ~/.lmstudio/bin
       end
       fish_add_path ~/.local/bin
+      if set -q XDG_CONFIG_HOME
+          set -gx PI_CODING_AGENT_DIR "$XDG_CONFIG_HOME/pi/agent"
+      else
+          set -gx PI_CODING_AGENT_DIR "$HOME/.config/pi/agent"
+      end
       if test -f ~/.config/fish/secrets.local.fish
           source ~/.config/fish/secrets.local.fish
       end
