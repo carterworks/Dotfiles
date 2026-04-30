b4101a626f45ecfc44d130fd30d1330b04db0233
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-04-30 13:11:30 -0600

    [nix] use home-manager on prostagma more

--- nix/users/root/home-manager.nix
+++ nix/users/root/home-manager.nix
@@ -5,10 +5,46 @@
     username = "root";
     homeDirectory = "/root";
     stateVersion = "25.11";
+    sessionVariables = {
+      EZA_ICON_SPACING = "2";
+    };
   };
 
   programs.home-manager.enable = true;
   programs.fish.enable = true;
+  programs.atuin.enable = true;
+  programs.eza = {
+    enable = true;
+    icons = "auto";
+    extraOptions = [
+      "--classify=auto"
+      "--group-directories-first"
+    ];
+  };
   programs.git.enable = true;
-  programs.helix.enable = true;
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor.whitespace.render = "all";
+      editor.indent-guides.render = true;
+      keys.normal = {
+        C-s = ":w";
+        C-x = ":q";
+      };
+    };
+  };
+  programs.zellij.enable = true;
+  programs.starship.enable = true;
+  programs.fzf.enable = true;
+  programs.zoxide = {
+    enable = true;
+    options = [
+      "--cmd"
+      "cd"
+    ];
+  };
+  programs.yazi = {
+    enable = true;
+    shellWrapperName = "yy";
+  };
 }
