959785ca41e3c1b80b85bec5f731e70baa8b0d72
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-05-26 21:11:51 -0600

    home: force disable hyprpaper service

--- nix/users/carter/home-manager.nix
+++ nix/users/carter/home-manager.nix
@@ -51,7 +51,7 @@ in
     };
   };
 
-  services.hyprpaper.enable = lib.mkIf pkgs.stdenv.isLinux false;
+  services.hyprpaper.enable = lib.mkForce false;
   services.wpaperd.enable = lib.mkIf pkgs.stdenv.isLinux true;
 
   home.stateVersion = "25.11";
