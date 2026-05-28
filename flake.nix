2f104e89954e56300dd25dd3bb739c9dd16ab985
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-05-28 09:11:39 -0600

    nixos: remove hyprland and cosmic, replace with kde

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org"
-      "https://hyprland.cachix.org"
       "https://cache.numtide.com"
       "https://vicinae.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -34,7 +31,6 @@
       url = "github:9001/copyparty";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
     vicinae.url = "github:vicinaehq/vicinae";
     stylix = {
       url = "github:nix-community/stylix";
