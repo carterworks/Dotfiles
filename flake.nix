1cd567a497adebc400841fb3e67e750e926541e1
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-06-01 10:06:45 -0600

    flake: pin home-manager release

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
