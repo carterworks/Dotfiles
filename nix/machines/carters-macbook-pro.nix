975f332ff8328614401cb7cd6b9d2520d4360d7e
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-07-09 09:37:57 -0600

    nix: always be deleting nix generations older than 7d

--- nix/machines/carters-macbook-pro.nix
+++ nix/machines/carters-macbook-pro.nix
@@ -7,5 +7,5 @@
   ids.gids.nixbld = 30000;
 
   nix.gc.automatic = true;
-  nix.gc.options = "--max-freed $((25 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))";
+  nix.gc.options = "--delete-older-than 7d";
 }
