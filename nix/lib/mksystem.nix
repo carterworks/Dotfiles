719b2f717a7b0b3fc1c95824ba9e80d343463c57
Carter McBride <18412686+carterworks@users.noreply.github.com>
2026-07-20 14:32:18 -0600

    nix: simplify system module arguments

--- nix/lib/mksystem.nix
+++ nix/lib/mksystem.nix
@@ -11,68 +11,42 @@ name:
   systemUsername ? profile,
   darwin ? false,
   gui ? true,
-  homeManager ? true,
   extraModules ? [ ],
 }:
 
 let
   lib = nixpkgs.lib;
   systemFunc = if darwin then inputs.nix-darwin.lib.darwinSystem else nixpkgs.lib.nixosSystem;
-  pkgsMaster = (inputs.nixpkgs-master or inputs.nixpkgs).legacyPackages.${system};
-  homeManagerModule = lib.optional homeManager (
+  homeManagerModule =
     if darwin then
       inputs.home-manager.darwinModules.home-manager
     else
-      inputs.home-manager.nixosModules.home-manager
-  );
+      inputs.home-manager.nixosModules.home-manager;
   userConfig = ../users/${profile}/${if darwin then "darwin" else "nixos"}.nix;
   homeConfig = ../users/${profile}/home-manager.nix;
 in
 systemFunc {
   inherit system;
+  specialArgs = { inherit inputs self systemUsername; };
 
   modules = [
-    {
-      _module.args = {
-        inherit
-          inputs
-          pkgsMaster
-          self
-          profile
-          systemUsername
-          gui
-          homeManager
-          ;
-        currentSystem = system;
-        currentSystemName = name;
-      };
-    }
     ../modules/nix.nix
   ]
   ++ lib.optional gui ../modules/fonts.nix
-  ++ homeManagerModule
-  ++ lib.optional homeManager {
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = false;
-      backupFileExtension = "hm-backup";
-      extraSpecialArgs = {
-        inherit
-          inputs
-          pkgsMaster
-          self
-          profile
-          systemUsername
-          gui
-          homeManager
-          ;
-        currentSystem = system;
-        currentSystemName = name;
-      };
-      users.${systemUsername} = homeConfig;
-    };
-  }
   ++ [
+    homeManagerModule
+    {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = false;
+        backupFileExtension = "hm-backup";
+        extraSpecialArgs = {
+          inherit inputs;
+          currentSystemName = name;
+        };
+        users.${systemUsername} = homeConfig;
+      };
+    }
     ../machines/${name}.nix
     ../users/${profile}/packages.nix
     userConfig
